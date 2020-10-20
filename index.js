3a1687ee2f5298d94a2a30e73244a27f81f64bed
yosh1 <yoppy1125yoppy@gmail.com>
2020-10-20 12:00:00 +0900

    Add socket.io

--- index.js
+++ index.js
@@ -5,29 +5,14 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 
-// import chirimen
-// https://tutorial.chirimen.org/raspi/nodejs
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-// GET http://localhost:3000/
-app.get('/',function(req,res){
-    res.json({
-        message:"Hello,world"
-    });
-});
-
-// GET http://localhost:3000/post
-app.post('/post', (req, res) => {
-  console.log(req.body);
-  res.send(req.json);
-  res.json({
-        message: req.body
-    });
+app.get('/' , function(req, res){
+   res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port);
