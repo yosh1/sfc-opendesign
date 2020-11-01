2a207b653a792a9d397fcda7d7bc42ead69e127b
yosh1 <yoppy1125yoppy@gmail.com>
2020-11-01 12:00:00 +0900

    Add socket.io (#2)

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const path = require('path');
 
-app.get('/' , function(req, res){
-   res.sendFile(__dirname + '/public/index.html');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/' , function(req, res, next){
+   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 io.on('connection',function(socket){
