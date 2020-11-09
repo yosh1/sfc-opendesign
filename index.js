01f3c10327601628a36fea65278ceacb2e97b9a9
yosh1 <yoppy1125yoppy@gmail.com>
2020-11-10 01:16:18 +0900

    Add socket.io

--- index.js
+++ index.js
@@ -3,17 +3,18 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/' , function(req, res){
    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(port);
-console.log('Listen on port: ' + port);
+io.on('connection',function(socket){
+    console.log('connected');
+});
+
+http.listen(PORT, function(){
+    console.log('Listen on port: ' + port);
+});
