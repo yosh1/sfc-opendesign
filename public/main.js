84e6bba9a6d4a9550960dcfc2c1b0c142a3bf7a3
yosh1 <yoppy1125yoppy@gmail.com>
2020-11-10 02:09:30 +0900

    Fix app name

--- public/main.js
+++ public/main.js
@@ -3,16 +3,9 @@ socket.on('connect', function () {
     console.log("Socket connected: " + socket.connected);
 });
 
-
-// もろもろの準備
 const video = document.getElementById("video");           // video 要素を取得
 const canvas = document.getElementById("canvas");         // canvas 要素の取得
 const context = canvas.getContext("2d");                  // canvas の context の取得
-
-var dat = document.getElementById("dat");
-var denden = document.getElementById("denden");
-
-const dendenImg = ["dendenAngry.png", "dendenDisgusted.png", "dendenFear.png", "dendenSad.png", "dendenSuprised.png", "dendenHappy.png", "dendenNormal.png"];
 
 // getUserMedia によるカメラ映像の取得
 var media = navigator.mediaDevices.getUserMedia({       // メディアデバイスを取得
@@ -60,12 +53,7 @@ function showData(pos) {
     str1 += "点" + i + ": ("
          + Math.round(pos[i][0]) + ", "                 // X座標（四捨五入して整数に）
          + Math.round(pos[i][1]) + ")<br>";             // Y座標（四捨五入して整数に）
-
-        //コンソールに表示
-        //  console.log("点" + i + "(" + Math.round(pos[i][0]) + "," + Math.round(pos[i][1]) + ")");
   }
-  // var dat1 = document.getElementById("dat1");             // データ表示用div要素の取得
-  // dat1.innerHTML = str1;                                  // データ文字列の表示
 }
 
 // ★感情データの表示
@@ -77,51 +65,3 @@ function showEmotionData(emo) {
   }
   dat.innerHTML = str2;                                  // データ文字列の表示
 }
-
-//電伝虫の顔を感情によって変える
-function changeDendenFace(emo){
-    var maxEmo = 6;
-    var maxEmoValue = 0;
-    for(var i = 0; i < emo.length; i++){
-        if(maxEmoValue < emo[i].value.toFixed(1)) {
-            maxEmoValue = emo[i].value.toFixed(1);
-            maxEmo = i;
-        }
-    }
-    denden.src = '/images/' + dendenImg[maxEmo];
-    console.log(emo[maxEmo].emotion + " : " + maxEmo);
-    // denden.src = "images/dendenAngry.png";
-
-}
-
-
-// function sendData(data){
-//   const targetUrl = 'https://sfc-opendesign.herokuapp.com/post';
-
-//   var xhr = new XMLHttpRequest();
-//   xhr.onreadystatechange = function()
-//     {
-//         var READYSTATE_COMPLETED = 4;
-//         var HTTP_STATUS_OK = 200;
-
-//         if( this.readyState == READYSTATE_COMPLETED
-//          && this.status == HTTP_STATUS_OK )
-//         {
-//             console.log( this.responseText );
-//         }
-//     }
-//   xhr.open( 'POST', targetUrl);
-//   xhr.setRequestHeader( 'Content-Type', 'application/json');
-//   xhr.send(JSON.stringify(data));
-// }
-
-// function generateData(emo){
-//   var sendData = "{";
-//   for(var i = 0; i < emo.length; i++) {                 // 全ての感情（6種類）について
-//     sendData += "\"" + emo[i].emotion + "\":" + emo[i].value.toFixed(1);
-//     if(i == emo.length-1) continue;
-//     sendData +=  ",";
-//   }
-//   sendData += "}"
-//   return sendData;
-// }
