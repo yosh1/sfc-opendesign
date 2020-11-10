b576ae340b73c7bf98afb931cb3b251b351ba086
yosh1 <yoppy1125yoppy@gmail.com>
2020-11-10 20:19:41 +0900

    Update obniz start / stop

--- obniz.js
+++ obniz.js
@@ -1,8 +1,18 @@
 'use strict';
 
 const Obniz = require("obniz");
-
 let obniz = new Obniz("76098301");
-obniz.onconnect = async function () {
 
+function obnizStart(){
+    obniz.onconnect = async function () {
+        obniz.io0.output(true);
+        obniz.io1.output(true);
+    }
+}
+
+function obnizStop() {
+    obniz.onconnect = async function () {
+        obniz.io0.end();
+        obniz.io1.end();
+    }
 }
